d5eb59bb7f4dddd11427d4e884397622c2c2aa50
ngan <vutran@sheridancollege.ca>
2025-10-22 18:08:10 -0400

    Custom Hook Practice: useFetch (finished)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { ToggleComponent } from "./ToggleComponent/ToggleComponent";
 import { SignupForm } from "./CustomHooks/SignupForm";
+import { useFetch } from "./CustomHooks/useFetch";
 
 function App() {
+  const { data, error, loading } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  data && console.log(data);
   return (
     <>
       <SignupForm />
